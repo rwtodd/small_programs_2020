5aafd749438979e59f5e5f675ed93c03d3a539d4
Richard Todd <rwtodd@users.noreply.github.com>
2020-11-22 20:32:52 -0600

    Added a readme and made the ffmpeg script fancier.

---
+++ README.md
@@ -0,0 +1,17 @@
+# 2020 Small Programs
+
+These are just random programs I didn't want to
+make whole repositories for.  I make a new repository once a year.
+
+- __archival__ some scripts
+- __calendar\_ppr__ groff report describing a derivation of a calendar calculation I needed for discordian dates.
+- __chaldean\_order__ some elisp code to play with planetary orderings
+- __djvu\_metadata__ scripts in golang and scala to facilitate the mass editing of DjVu file metadata.
+- __elisp\_fragments__ just what it says on the tin
+- __ffmpeg-transcode__ powershell script to facilitate converting some movies I had with ffmpeg.
+- __geomancy-totals__ elisp code to tally geomantic figures
+- __music-scripts__ various batch, powershell, and bash scripts which are variations on how I like to convert FLAC files for portable listening.
+- __qs\_downloader__ bash scripts to make it ease to (re)download the various vorbis and MP3 files I like from Quest Studios.  It was an exercise in using base arrays and maps.
+- __schillinger__ playing with lua coroutines and schillinger music patterns.
+- __xwin1__ a very basic x-windows program in C
+
--- ffmpeg-transcode/movie-cvt.ps1
+++ ffmpeg-transcode/movie-cvt.ps1
@@ -4,7 +4,8 @@ This is a script to remove some tedium from the process of transcoding
 DVDs to H.265+libopus MKV files.
 
 .Description
-ffmpeg and ffprobe are used in this process
+ffmpeg and ffprobe are used in this process. The current directory is always the
+output location.
 
 .Parameter movie
 The movie to transcode
@@ -15,32 +16,40 @@ Map all the input streams
 
 .Example
 movie-cvt.ps1 -movie my-file.mkv
+.Example
+gci ..\*.mkv | movie-cvt.ps1 -mapall
 #>
 Param (
-   [Parameter(Mandatory=$true)][System.IO.FileInfo] $movie,
+   [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+   [ValidatePattern('\.mkv$')]
+   [System.IO.FileInfo] $movie,
    [Switch] $mixdown,
    [Switch] $mapall
 )
+BEGIN {
+   $probe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\ffprobe.exe"
+   $mpeg = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\ffmpeg.exe"
 
-$probe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\ffprobe.exe"
-$mpeg = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\ffmpeg.exe"
+   $mix = ""
+   if ($mixdown) {
+      $mix = "-ac 2"
+   }
 
-$result = $movie.BaseName + "_small.mkv"
-echo "Writing result to $result"
-
-$streams = "-map 0"
-
-if(-not $mapall) {
-  & $probe -i $movie
-  $streams = "0 " + (Read-Host -Prompt "Which streams to map? (0 is a given) ")
-  $streams = ($streams -split '\s+') | ForEach-Object -Process { "-map 0:" + $_ }
 }
+PROCESS {
+   Write-Verbose "Input movie is $movie"
+   $result = $movie.BaseName + "_small.mkv"
+   Write-Information "Writing result to $result"
 
-$mix = ""
-if($mixdown) {
-   $mix = "-ac 2"
+   $streams = "-map 0"
+   if (-not $mapall) {
+      & $probe -i $movie
+      $streams = "0 " + (Read-Host -Prompt "Which streams to map? (0 is a given) ")
+      $streams = ($streams.Trim() -split '\s+') | ForEach-Object -Process { "-map 0:" + $_ }
+   }
+
+   $cmd = "& `"$mpeg`" -i `"$movie`" -c:s copy -c:v libx265 -crf 23 -c:a libopus -b:a 128k $streams $mix `"$result`""
+   Write-Verbose $cmd
+   Invoke-Expression $cmd
 }
-
-$cmd = "& `"$mpeg`" -i `"$movie`" -c:s copy -c:v libx265 -crf 23 -c:a libopus -b:a 128k $streams $mix `"$result`""
-echo $cmd
-Invoke-Expression $cmd
+END {}
