1e295c95d946e7fb19c930c96a9ff1279322d8fc
Richard Todd <rwtodd@users.noreply.github.com>
2020-11-23 17:24:34 -0600

    Did some fairly intrusive changes to optimize
    Converted the functions into static methods, and avoided
    pipelines and array-creation.  It still takes 1.1 minutes,
    and I haven't measured recently but I remember the
    elisp version being a _lot_ snappier.  That's disappointing.

--- geomancy-totals/geotots.ps1
+++ geomancy-totals/geotots.ps1
@@ -8,58 +8,71 @@
 # at present.
 
 $fig_names = @{
-    15="Via";
-    14="Cauda Draconis";
-    13="Puer";
-    12="Fortuna Minor";
-    11="Puella";
-    10="Amissio";
-    9 ="Carcer";
-    8 ="Laetitia";
-    7 ="Caput Draconis";
-    6 ="Conjunctio";
-    5 ="Acquisitio";
-    4 ="Rubeus";
-    3 ="Fortuna Major";
-    2 ="Albus";
-    1 ="Tristitia";
-    0 ="Populus"
+    15 = "Via";
+    14 = "Cauda Draconis";
+    13 = "Puer";
+    12 = "Fortuna Minor";
+    11 = "Puella";
+    10 = "Amissio";
+    9  = "Carcer";
+    8  = "Laetitia";
+    7  = "Caput Draconis";
+    6  = "Conjunctio";
+    5  = "Acquisitio";
+    4  = "Rubeus";
+    3  = "Fortuna Major";
+    2  = "Albus";
+    1  = "Tristitia";
+    0  = "Populus"
 }
 
-function add-figures([int]$one,[int]$two) { $one -bxor $two }
-function get-lineof([int]$fig,[int]$ln) { 1 -band ($fig -shr (4-$ln)) }
+class GeomanticTotals {
+    static [int] AddFigures([int]$one, [int]$two) { return ($one -bxor $two) }
+    static [int] LineOf([int]$fig, [int]$ln) { return (1 -band ($fig -shr (4 - $ln))) }
 
-function new-figure([int[]] $lns) {
-    (($lns[0] -shl 3) -bor ($lns[1] -shl 2) -bor
-        ($lns[2] -shl 1) -bor $lns[3])
-}
+    static [int] NewFigure([int[]] $lns) {
+        return (($lns[0] -shl 3) -bor ($lns[1] -shl 2) -bor
+            ($lns[2] -shl 1) -bor $lns[3])
+    }
 
-function new-sisters([int[]]$moms) {
-    (new-figure ($moms | %{ get-lineof $_ 1 })),
-    (new-figure ($moms | %{ get-lineof $_ 2 })),
-    (new-figure ($moms | %{ get-lineof $_ 3 })),
-    (new-figure ($moms | %{ get-lineof $_ 4 }))
-}
+    static [int[]] NewSisters([int[]]$moms) {
+        $ans = [int[]]::new(4)
+        $tmp = [int[]]::new(4)
+        for($idx = 0; $idx -lt 4; $idx++) {
+            $tmp[0] = [GeomanticTotals]::LineOf($moms[0], $idx+1)
+            $tmp[1] = [GeomanticTotals]::LineOf($moms[1], $idx+1)
+            $tmp[2] = [GeomanticTotals]::LineOf($moms[2], $idx+1)
+            $tmp[3] = [GeomanticTotals]::LineOf($moms[3], $idx+1)
+            $ans[$idx] = [GeomanticTotals]::NewFigure($tmp)
+        }
+        return $ans
+    }
 
-function new-shield([int[]]$moms) {
-    $sis = new-sisters $moms
-    $nephs = (add-figures $moms[0] $moms[1]), 
-             (add-figures $moms[2] $moms[3]),
-             (add-figures $sis[0] $sis[1]),
-             (add-figures $sis[2] $sis[3])
-    $wits = (add-figures $nephs[2] $nephs[3]),
-            (add-figures $nephs[0] $nephs[1])
-    $j = add-figures $wits[0] $wits[1]
-    $script:judge[$fig_names[$j]]++
+    static [void] NewShield([int[]]$moms) {
+        $sis = [GeomanticTotals]::NewSisters($moms)
+        $n1 = [GeomanticTotals]::AddFigures($moms[0], $moms[1])
+        $n2 = [GeomanticTotals]::AddFigures($moms[2], $moms[3])
+        $n3 = [GeomanticTotals]::AddFigures($sis[0], $sis[1])
+        $n4 = [GeomanticTotals]::AddFigures($sis[2], $sis[3])
+        $wr = [GeomanticTotals]::AddFigures($n3, $n4)
+        $wl = [GeomanticTotals]::AddFigures($n1, $n2)
+        $j = [GeomanticTotals]::AddFigures($wr,$wl)
+        $script:judge[$script:fig_names[$j]]++
+    }
 }
 
 $judge = @{ }
-foreach($m1 in 0..15) {
-    foreach($m2 in 0..15) {
+$mothers = [int[]]::new(4)
+foreach ($m1 in 0..15) {
+    $mothers[0] = $m1
+    foreach ($m2 in 0..15) {
         Write-Host "on level $m1 $m2 xx xx"
-        foreach($m3 in 0..15) {
-            foreach($m4 in 0..15) {
-                new-shield $m1,$m2,$m3,$m4
+        $mothers[1] = $m2
+        foreach ($m3 in 0..15) {
+            $mothers[2] = $m3
+            foreach ($m4 in 0..15) {
+                $mothers[3] = $m4
+                [GeomanticTotals]::NewShield($mothers)
             }
         }
     }
