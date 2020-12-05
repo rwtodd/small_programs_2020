6139d1a828f8ebb53c865b4fee549323373052ac
Richard Todd <rwtodd@users.noreply.github.com>
2020-12-04 21:42:12 -0600

    threaded jobs for flac-to-opus.ps1

--- music-scripts/flac-to-opus.ps1
+++ music-scripts/flac-to-opus.ps1
@@ -1,12 +1,13 @@
 # convert the flac files in the current directory to opus files at
-# $bitrate (default 256)
+# bitrate 256
 
 $oe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\opusenc.exe"
-$bitrate = 256
 
-get-childitem *.flac | %{
-    echo $_.Name
-    & $oe --quiet --bitrate $bitrate $_ ($_ -replace 'flac$','opus')
-    remove-item -LiteralPath $_
+get-childitem *.flac | %{ 
+   Start-ThreadJob -ScriptBlock {
+     param($infile)
+     opusenc --bitrate 256 $infile ($infile.BaseName + ".opus")
+     remove-item -LiteralPath $infile
+   } -ThrottleLimit 3 -Name $_  -ArgumentList $_ 
 }
 
