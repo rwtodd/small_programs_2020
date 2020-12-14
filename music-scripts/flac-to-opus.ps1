62282cce67163b04501b0c1ee0686185620b0f86
Richard Todd <rwtodd@users.noreply.github.com>
2020-12-13 21:26:48 -0600

    tweaked flac-to-opus script again

--- music-scripts/flac-to-opus.ps1
+++ music-scripts/flac-to-opus.ps1
@@ -2,12 +2,13 @@
 # bitrate 256
 
 $oe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\opusenc.exe"
+$bitrate = 256
 
 get-childitem *.flac | %{ 
    Start-ThreadJob -ScriptBlock {
-     param($infile)
-     opusenc --bitrate 256 $infile ($infile.BaseName + ".opus")
-     remove-item -LiteralPath $infile
-   } -ThrottleLimit 3 -Name $_  -ArgumentList $_ 
+       param($opus,$brate,$infile)
+       & $opus --bitrate $brate $infile ($infile.BaseName + ".opus")
+       remove-item -LiteralPath $infile
+   } -ThrottleLimit 3 -Name $_  -ArgumentList @($oe,$bitrate,$_) 
 }
 
