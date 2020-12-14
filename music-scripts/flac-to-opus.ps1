54b796c2395e5d26722afcc13fdefa899b42dcfe
Richard Todd <rwtodd@users.noreply.github.com>
2020-12-14 12:11:58 -0600

    updated parallel music conversion scripts

--- music-scripts/flac-to-opus.ps1
+++ music-scripts/flac-to-opus.ps1
@@ -1,14 +1,29 @@
-# convert the flac files in the current directory to opus files at
-# bitrate 256
+<#
+.Synopsis
+Then, convert the flac files in the current directory to opus.
+
+.Description
+opusenc is used to convert the flac files.  The default bitrate is 256.  The files
+are converted in parallel, three at a time.
+
+.Parameter BitRate
+The output bitrate (default 256).
+
+.Example
+flac-to-opus.ps1 -BitRate 256
+#>
+Param(
+    [int]$BitRate = 256
+)
 
 $oe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\opusenc.exe"
-$bitrate = 256
 
-get-childitem *.flac | %{ 
-   Start-ThreadJob -ScriptBlock {
-       param($opus,$brate,$infile)
-       & $opus --bitrate $brate $infile ($infile.BaseName + ".opus")
-       remove-item -LiteralPath $infile
-   } -ThrottleLimit 3 -Name $_  -ArgumentList @($oe,$bitrate,$_) 
-}
+$oldSize = (get-childitem *.flac | measure-object Length -Sum).Sum
+get-childitem *.flac | ForEach-Object -Parallel {
+    Write-Host "Converting $_" 
+    & $Using:oe --quiet --bitrate $Using:BitRate $_ ($_.BaseName + ".opus")
+    remove-item -LiteralPath $_
+} -ThrottleLimit 3
 
+$newSize = (get-childitem *.opus | measure-object Length -Sum).Sum
+Write-Host "Size is now $(100*$newSize/$oldSize)% of the original size."
--- music-scripts/zip-to-opus.ps1
+++ music-scripts/zip-to-opus.ps1
@@ -1,34 +1,45 @@
 <#
 .Synopsis
-Open the provided ZIP file into a directory of the same name in the current
-directory.  Then, convert the flac files in the current directory to opus
-files at $bitrate (default 256)
+Extract files from a zip archive, and convert flac files to opus.
 
 .Description
 opusenc is used to convert the flac files.
+The given Zip file is expanded into a directory of the same name as the archive in the current
+directory.  Then, all flac files in the new directory are converted to opus
+files at the given bit rate (default 256)
 
-.Parameter zip
+.Parameter ZipFile
 The zip file to decompress
-.Parameter bitrate
+.Parameter BitRate
 The output bitrate (default 256).
 
 .Example
-zip-to-opus.ps1 -zip a_zipfile.zip -bitrate 256
+zip-to-opus.ps1 a_zipfile.zip -BitRate 256
 #>
 Param (
-   [Parameter(Mandatory=$true)][System.IO.FileInfo] $zip,
-   [Int32] $bitrate = 256
+   [Parameter(Position=0,Mandatory=$true)]
+   [ValidatePattern("zip$")]
+   [string] $ZipFile,
+   [Int32] $BitRate = 256
 )
-
 $oe = "C:\Program Files\ffmpeg-20200831-4a11a6f-win64-static\bin\opusenc.exe"
+
+$zip = (get-item $ZipFile)
 $album = $zip.BaseName
 
 mkdir $album | Out-Null
 push-location $album | Out-Null
+
 Expand-Archive -LiteralPath $zip -DestinationPath .
-get-childitem *.flac | %{
-    echo $_.Name
-    & $oe --quiet --bitrate $bitrate $_ ($_ -replace 'flac$','opus')
+$oldSize = [Math]::Max( (get-childitem . | measure-object Length -Sum).Sum, 1 )
+
+get-childitem *.flac | ForEach-Object -Parallel {
+    Write-Host "Converting $($_.Name)" 
+    & $Using:oe --quiet --bitrate $Using:BitRate $_ ($_.BaseName + ".opus")
     remove-item -LiteralPath $_
-}
+} -ThrottleLimit 3
+
+$newSize = (get-childitem . | measure-object Length -Sum).Sum
+Write-Host "Size is now $(100*$newSize/$oldSize)% of the original size."
+
 pop-location
