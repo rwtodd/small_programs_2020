4e4ff90e4bb282c02446368efa91a181a9ffadfd
Richard Todd <rwtodd@users.noreply.github.com>
2021-01-03 23:09:17 -0600

    added second elisp version

--- digit_counts/measure-digits.el
+++ digit_counts/measure-digits.el
@@ -5,5 +5,15 @@
   (let ((histo (make-hash-table)))
     (seq-doseq (chr (number-to-string n))
       (puthash chr (1+ (gethash chr histo 0)) histo))
-    (dolist (key (hash-table-keys histo))
-      (princ (format "%s: %d\n" (char-to-string key) (gethash key histo))))))
+    (mapcar #'(lambda (key)
+		(list (char-to-string key)
+		      (gethash key histo)))
+	    (hash-table-keys histo))))
+
+(defun histo-2 (n)
+  "Compute the count of each digit in the given number. This time use
+built-in seq-group-by function"
+  (mapcar #'(lambda (bin)
+	      (list (char-to-string (car bin))
+		    (length (cdr bin))))
+	  (seq-group-by #'identity (number-to-string n))))
