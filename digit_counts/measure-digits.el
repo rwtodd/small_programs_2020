54a08f287a65fd4a95a59910200f17f3b8986f3c
Richard Todd <rwtodd@users.noreply.github.com>
2021-01-03 23:32:12 -0600

    added example code for exploring measured digits

--- digit_counts/measure-digits.el
+++ digit_counts/measure-digits.el
@@ -17,3 +17,10 @@ built-in seq-group-by function"
 	      (list (char-to-string (car bin))
 		    (length (cdr bin))))
 	  (seq-group-by #'identity (number-to-string n))))
+
+;; to find numbers which have the same number of each digit...
+;; (defun same-digits-p (x) (apply #'= (mapcan #'cdr (histo x))))
+;; (cl-loop for x from 1 to 400 appending
+;; 	 (cl-loop for y from 1 to 400
+;; 		  when (same-digits-p (expt x y))
+;; 		  collect (list x y (expt x y))))
